28a428488d6a5506e90b2631bb2274c68f4b6256
Anees A L <aneesal874@gmail.com>
2026-03-07 16:57:56 +0530

    small fixes in frontend for crediential

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+
+
+ axios.defaults.withCredentials = true
 createRoot(document.getElementById('root')).render(
+ 
   <BrowserRouter  >
   <App />
   </BrowserRouter>
