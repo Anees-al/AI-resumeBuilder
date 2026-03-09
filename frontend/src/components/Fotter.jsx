7afd6f033ccc4f398143e78ed78a45d5614491f3
Anees A L <aneesal874@gmail.com>
2026-03-09 22:25:54 +0530

    new footer added

--- frontend/src/components/Fotter.jsx
+++ frontend/src/components/Fotter.jsx
@@ -2,17 +2,25 @@ import React from 'react'
 
 const Fotter = () => {
   return (
-    <footer className="w-full bg-gradient-to-b from-indigo-200 to-[#FFFFFF] text-gray-800 mt-12">
-            <div className="max-w-7xl mx-auto px-6 py-16 flex flex-col items-center">
+    <footer className="w-full bg-[#7978FA] text-gray-800 mt-12">
+            <div className="max-w-7xl mx-auto px-6 py-16 flex sm:flex-row items-center justify-between gap-10">
                 <div className="flex items-center space-x-3 mb-6">
-                    <h1 className='text-3xl font-semibold'>Resume</h1>
+                    <h1 className='text-3xl font-bold text-white'>Create AI assisted resumes</h1>
                 </div>
-                <p className="text-center max-w-xl text-sm font-normal leading-relaxed">
-                    Create a future-ready resume powered by smart technology and real insights.
-                </p>
+
+
+                <div className='hidden sm:flex flex-col text-sm  text-white mt-20 gap-3'>
+                    <p className='cursor-pointer'>About</p>
+                    <p className='cursor-pointer'>Templates</p>
+                    <p className='cursor-pointer'>Resume</p>
+                </div>
+
+
+                <p className='hidden sm:flex text-white max-w-[400px] ml-10 text-semibold'>This resume was created with the assistance of AI tools and reviewed for accuracy by the candidate.</p>
+                
             </div>
-            <div className="border-t border-slate-400">
-                <div className="max-w-7xl mx-auto px-6 py-6 text-center text-sm font-normal">
+            <div className="border-t border-slate-200">
+                <div className="max-w-7xl mx-auto px-6 py-6 text-center text-sm font-normal text-white font-semibold">
                  ©2025. All rights reserved.
                 </div>
             </div>
