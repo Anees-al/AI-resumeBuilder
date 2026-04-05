d5f45c2c989d5fafd4edfeabf978d149f15faebf
Anees A L <aneesal874@gmail.com>
2026-04-05 14:53:04 +0530

    small changes in the feature components

--- frontend/src/components/Features.jsx
+++ frontend/src/components/Features.jsx
@@ -21,15 +21,18 @@ const Features = () => {
                <motion.div className='flex flex-col gap-3 ' initial={{y:100,opacity:0}} whileInView={{y:0,opacity:1}}  transition={{duration:0.6}}>
                      <Camera  className='text-green-500' />
 
-                     <motion.h1 className='text-2xl font-black max-w-[200px] playfair' initial={{y:100,opacity:0}} whileInView={{y:0,opacity:1}}  transition={{duration:0.6}}>Recruiter-Approved Resume</motion.h1>
-                     <motion.p className='text-lg font-semibold text-gray-700 max-w-[220px] playfair' initial={{y:100,opacity:0}} whileInView={{y:0,opacity:1}}  transition={{duration:0.6}}>We work with recruiters to design resume templates that format automatically.</motion.p>
+                     <motion.h1 className='text-2xl font-black max-w-[200px] playfair' initial={{y:100,opacity:0}} whileInView={{y:0,opacity:1}}  transition={{duration:0.6}}>Different Templates</motion.h1>
+                     <motion.p className='text-lg font-semibold text-gray-700 max-w-[220px] playfair' initial={{y:100,opacity:0}} whileInView={{y:0,opacity:1}}  transition={{duration:0.6}}>
+
+Pick from multiple professional resume templates suited to various careers, ensuring a clean and tailored look.
+</motion.p>
 
                 </motion.div>
                 <motion.div className='flex flex-col gap-3 ' initial={{y:100,opacity:0}} whileInView={{y:0,opacity:1}}  transition={{duration:0.6}}>
                      <Camera  className='text-green-500' />
 
-                     <motion.h1 className='text-2xl font-black max-w-[200px] playfair' initial={{y:100,opacity:0}} whileInView={{y:0,opacity:1}}  transition={{duration:0.6}}>Recruiter-Approved Resume</motion.h1>
-                     <motion.p className='text-lg font-semibold text-gray-700 max-w-[220px] playfair' initial={{y:100,opacity:0}} whileInView={{y:0,opacity:1}}  transition={{duration:0.6}}>We work with recruiters to design resume templates that format automatically.</motion.p>
+                     <motion.h1 className='text-2xl font-black max-w-[200px] playfair' initial={{y:100,opacity:0}} whileInView={{y:0,opacity:1}}  transition={{duration:0.6}}>AI-Assisted Enhancement</motion.h1>
+                     <motion.p className='text-lg font-semibold text-gray-700 max-w-[220px] playfair' initial={{y:100,opacity:0}} whileInView={{y:0,opacity:1}}  transition={{duration:0.6}}>Improve your resume instantly with smart AI suggestions that refine wording, highlight strengths, and boost overall impact.</motion.p>
 
                 </motion.div>
                 
